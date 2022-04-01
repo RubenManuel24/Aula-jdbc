85953c981f0be07fa916e5c7f2fef932593d8cf9
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-04-02 00:59:05 +0100

    Department entity class

---
+++ src/aplication/Program.java
@@ -0,0 +1,14 @@
+package aplication;
+
+import model.entities.Department;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Department obj = new Department(1,"Frutas");
+		
+		System.out.println(obj.toString());
+	}
+
+}
--- src/db/DB.java
+++ src/db/DB.java
@@ -1,4 +1,4 @@
-package db;
+ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
