f4802b095efa94443be20526fd462662697a7810
Ruben Manuel <rubenadilsonsapalomanuel@gmail.com>
2022-04-02 01:47:22 +0100

    Seller entity class

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,14 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department obj = new Department(2,"Angola");
-		
-		System.out.println(obj.toString());
+		Department obj = new Department(2,"Book");
+		Seller seller = new Seller(23,"Ruben","ruben@gmail.com",new Date(),100000.00, obj );
+		System.out.println(seller.toString());
 	}
 
 }
